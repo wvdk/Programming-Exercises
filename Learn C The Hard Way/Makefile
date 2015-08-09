026e05837377d84567c55df6614e82fdbeaa8141
wvdk <wes.vdk@icloud.com>
2015-08-09 13:36:29 -0700

    Adding LCTHW

---
+++ Learn C The Hard Way/Makefile
@@ -0,0 +1,9 @@
+CFLAGS=-Wall -g
+
+all:
+	make ex1
+	make ex3
+
+clean:
+	rm -f ex1
+	rm -f ex3
---
+++ Learn C The Hard Way/ex1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  puts("Hello wold.");
+
+  return 0;
+}
