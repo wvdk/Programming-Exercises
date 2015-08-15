9ca5b7be398f5a00fcc9b90ca1eef69443011227
wvdk <wes.vdk@icloud.com>
2015-08-15 10:03:07 -0700

    Init new project: Exercises in Programming Style

---
+++ Exercises in Programming Style/10.1/things.swift
@@ -0,0 +1,23 @@
+#!/usr/bin/env swift
+
+import Foundation
+
+
+///////////////////////
+// CLASSES
+///////////////////////
+
+class DataStorageManager(){}
+
+class StopWordManager(){}
+
+class WordFrequencyManager(){}
+
+class WordFrequencyController(){}
+
+
+/////////////////////////
+// MAIN FUNCTION
+/////////////////////////
+
+//WordFrequencyController(Process.arguments[1]).run()
