7969ff48d9c874dd1c961583ec18c7531a40bccc
wvdk <wes.vdk@icloud.com>
2015-08-15 13:42:55 -0700

    Fix syntax bugs and added test file.

--- Exercises in Programming Style/10.1/things.swift
+++ Exercises in Programming Style/10.1/things.swift
@@ -7,13 +7,37 @@ import Foundation
 // CLASSES
 ///////////////////////
 
-class DataStorageManager(){}
+class DataStorageManager {
+    var data: String?
+    var fm = NSFileManager.defaultManager()
+    
+    init(pathToFile: String) {
+        let fileData = fm.contentsAtPath(pathToFile)
+	if let contentsOfFile = NSString(data: fileData!, encoding: NSUTF8StringEncoding) as String? {
+	   print(contentsOfFile)
+	}
+    }
+    
+    func words() -> [String] {
+        var arrayOfWords: [String]
+        
+        arrayOfWords = ["Test", "Stuff", "foo", "bar"]
+        
+        return arrayOfWords
+    }
+}
 
-class StopWordManager(){}
+class StopWordManager {
+    
+}
 
-class WordFrequencyManager(){}
+class WordFrequencyManager {
+    
+}
 
-class WordFrequencyController(){}
+class WordFrequencyController {
+    
+}
 
 
 /////////////////////////
@@ -21,3 +45,10 @@ class WordFrequencyController(){}
 /////////////////////////
 
 //WordFrequencyController(Process.arguments[1]).run()
+
+
+////////////////////////
+// TESTS
+////////////////////////
+
+let t = DataStorageManager(pathToFile: Process.arguments[1])
