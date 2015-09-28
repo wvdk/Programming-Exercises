0a008a29d8c0a820a3650389869dbcd20554edc3
wvdk <wes.vdk@icloud.com>
2015-09-28 03:17:51 -0700

    Learning Arduino project started

---
+++ Learning Arduino/_02_Temperature_Sensor/_02_Temperature_Sensor.ino
@@ -0,0 +1,51 @@
+const int sensorPin = A0;
+
+const float baselineTemp = 27.0;
+
+void setup() {
+  Serial.begin(9600);
+  
+  for (int pinNumber = 2; pinNumber < 5; pinNumber++) {
+    pinMode(pinNumber, OUTPUT);
+    digitalWrite(pinNumber, LOW);
+  }
+  
+}
+
+void loop() {
+  
+  int sensorVal = analogRead(sensorPin);
+  
+  Serial.print("Sensor Value: ");
+  Serial.print(sensorVal);
+  
+  float voltage = (sensorVal/1024.0) * 5.0;
+  
+  Serial.print(", Volts: ");
+  Serial.print(voltage);
+  
+  Serial.print(", Degrees C: ");
+  float temperature = (voltage - 0.5) * 100;
+  Serial.println(temperature);
+  
+  if (temperature < baselineTemp) {
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW); 
+  } else if (temperature >= baselineTemp + 1 && temperature < baselineTemp + 2) {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW); 
+  } else if (temperature >= baselineTemp + 2 && temperature < baselineTemp + 3) {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, LOW); 
+  } else if (temperature >= baselineTemp + 3) {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+  }
+  
+  delay(1);
+}
+
