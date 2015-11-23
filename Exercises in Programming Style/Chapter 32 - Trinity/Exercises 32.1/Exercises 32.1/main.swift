2eb866f3ccba01b2851aa173552b73302dbf2560
wvdk <wes.vdk@icloud.com>
2015-11-23 12:10:48 -0800

    init project for chapter 32

---
+++ Exercises in Programming Style/Chapter 32 - Trinity/Exercises 32.1/Exercises 32.1/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  Exercises 32.1
+//
+//  Created by Wesley Van der Klomp on 11/23/15.
+//  Copyright © 2015 Wes Van der Klomp. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+print("You're path: \(Process.arguments[1])")
