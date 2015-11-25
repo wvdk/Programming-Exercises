ba44d83ebc01dd3c1ab93ba05bc6686b2d9032bd
wvdk <wes.vdk@icloud.com>
2015-11-25 15:53:15 -0800

    NSDate formating for ISO-8601

---
+++ Learning Swift and Cocoa/NSDate formating.playground/Contents.swift
@@ -0,0 +1,31 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let originalDate = "2016-10-14T00:58:56.851Z"
+
+public extension NSDate {
+    public class func ISOStringFromDate(date: NSDate) -> String {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
+        dateFormatter.timeZone = NSTimeZone(abbreviation: "GMT")
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS"
+        
+        return dateFormatter.stringFromDate(date).stringByAppendingString("Z")
+    }
+    
+    public class func dateFromISOString(string: String) -> NSDate {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
+        dateFormatter.timeZone = NSTimeZone.localTimeZone()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+        
+        return dateFormatter.dateFromString(string)!
+    }
+}
+
+NSDate.ISOStringFromDate(NSDate())
+
+let newDate = NSDate.dateFromISOString(originalDate)
+
+NSDate.ISOStringFromDate(newDate)
