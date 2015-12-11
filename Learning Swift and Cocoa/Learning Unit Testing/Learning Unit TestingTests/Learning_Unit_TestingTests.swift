1188ae6bf2bfd2439f7299bbdc8806b4afab5566
wvdk <wes.vdk@icloud.com>
2015-12-11 15:31:50 -0800

    first test

--- Learning Swift and Cocoa/Learning Unit Testing/Learning Unit TestingTests/Learning_Unit_TestingTests.swift
+++ Learning Swift and Cocoa/Learning Unit Testing/Learning Unit TestingTests/Learning_Unit_TestingTests.swift
@@ -11,26 +11,38 @@ import XCTest
 
 class Learning_Unit_TestingTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
+//    override func setUp() {
+//        super.setUp()
+//        // Put setup code here. This method is called before the invocation of each test method in the class.
+//    }
+//    
+//    override func tearDown() {
+//        // Put teardown code here. This method is called after the invocation of each test method in the class.
+//        super.tearDown()
+//    }
     
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
+        let myClass = MyClassToTest(thing: "Hello")
+        
+        var thing: String = myClass.thisIsAPropertyThatMustExist
+        
+        thing = ""
+        
+        if thing.characters.count != 0 {
+            XCTAssert(true, "good")
+        } else {
+            XCTAssert(false)
         }
+//        XCTAssert(myClass.thisIsAMethodToTest())
     }
     
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measureBlock {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
+//    
 }
