cff3968e675fe9fa47398715a7d7eafe9621b674
wvdk <wes.vdk@icloud.com>
2015-12-11 11:56:43 -0800

    Create class to test

---
+++ Learning Swift and Cocoa/Learning Unit Testing/Learning Unit Testing/MyClassToTest.swift
@@ -0,0 +1,13 @@
+//
+//  MyClassToTest.swift
+//  Learning Unit Testing
+//
+//  Created by Wesley Van der Klomp on 12/11/15.
+//  Copyright © 2015 Wes Van der Klomp. All rights reserved.
+//
+
+import UIKit
+
+class MyClassToTest: NSObject {
+
+}
