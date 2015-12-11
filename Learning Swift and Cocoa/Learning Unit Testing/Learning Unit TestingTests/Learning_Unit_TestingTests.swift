55ca2505a22ec30416aa438c9ff83ac5f79a06ac
wvdk <wes.vdk@icloud.com>
2015-12-11 11:52:11 -0800

    Init learning unit testing project

---
+++ Learning Swift and Cocoa/Learning Unit Testing/Learning Unit TestingTests/Learning_Unit_TestingTests.swift
@@ -0,0 +1,36 @@
+//
+//  Learning_Unit_TestingTests.swift
+//  Learning Unit TestingTests
+//
+//  Created by Wesley Van der Klomp on 12/11/15.
+//  Copyright © 2015 Wes Van der Klomp. All rights reserved.
+//
+
+import XCTest
+@testable import Learning_Unit_Testing
+
+class Learning_Unit_TestingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
