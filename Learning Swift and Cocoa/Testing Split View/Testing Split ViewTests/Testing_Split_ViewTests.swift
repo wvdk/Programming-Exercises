215f1c85681a263b4d520f7efb9ed98e040cb896
wvdk <wes.vdk@icloud.com>
2016-02-08 18:18:42 -0800

    new project

---
+++ Learning Swift and Cocoa/Testing Split View/Testing Split ViewTests/Testing_Split_ViewTests.swift
@@ -0,0 +1,36 @@
+//
+//  Testing_Split_ViewTests.swift
+//  Testing Split ViewTests
+//
+//  Created by Wesley Van der Klomp on 2/8/16.
+//  Copyright © 2016 Wes Van der Klomp. All rights reserved.
+//
+
+import XCTest
+@testable import Testing_Split_View
+
+class Testing_Split_ViewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
