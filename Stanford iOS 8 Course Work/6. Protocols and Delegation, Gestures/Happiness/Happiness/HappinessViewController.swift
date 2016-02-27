c740f61608760e78baa1ce6668c3f3202a03694c
wvdk <wes.vdk@icloud.com>
2016-02-27 10:42:11 -0800

    duplicate project because was missing in folder for Ch. 6

---
+++ Stanford iOS 8 Course Work/6. Protocols and Delegation, Gestures/Happiness/Happiness/HappinessViewController.swift
@@ -0,0 +1,55 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Wesley Van der Klomp on 2/21/16.
+//  Copyright © 2016 Wesley Van der Klomp. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController, FaceViewDataSource {
+
+    @IBOutlet weak var faceView: FaceView! {
+        didSet {
+            faceView.dataSource = self
+            faceView.addGestureRecognizer(UIPinchGestureRecognizer(target: faceView, action: "scale:"))
+        }
+    }
+    
+    private struct Constants {
+        static let HappinessGestureScale: CGFloat = 4
+    }
+    
+    @IBAction func changeHappiness(gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .Ended: fallthrough
+        case .Changed:
+            let translation = gesture.translationInView(faceView)
+            let happinessChange = -Int(translation.y / Constants.HappinessGestureScale)
+            if happinessChange != 0 {
+                happiness += happinessChange
+                gesture.setTranslation(CGPointZero, inView: faceView)
+            }
+        default: break
+        }
+    }
+    
+    // Model
+    var happiness: Int = 60 { // 0 is very sad, 100 is super happy
+        didSet {
+            happiness = min(max(happiness, 0), 100)
+            print("happiness = \(happiness)")
+            updateUI()
+        }
+    }
+    
+    func updateUI() {
+        faceView.setNeedsDisplay()
+    }
+    
+    func smilinessForFaceView(sender: FaceView) -> Double? {
+        return Double(happiness-50)/50
+    }
+
+}
