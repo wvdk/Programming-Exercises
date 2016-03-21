36acf811915b678eba18ddc92b39b11e46b215f1
wvdk <wes.vdk@icloud.com>
2016-03-20 17:02:05 -0700

    edits

---
+++ Learning Swift and Cocoa/Bottom Refresh in Swift/Bottom Refresh in Swift/LoadingTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  LoadingTableViewCell.swift
+//  Bottom Refresh in Swift
+//
+//  Created by Wesley Van der Klomp on 3/20/16.
+//  Copyright © 2016 Wesley Van der Klomp. All rights reserved.
+//
+
+import UIKit
+
+class LoadingTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+
+}
--- Learning Swift and Cocoa/Bottom Refresh in Swift/Bottom Refresh in Swift/ViewController.swift
+++ Learning Swift and Cocoa/Bottom Refresh in Swift/Bottom Refresh in Swift/ViewController.swift
@@ -10,16 +10,76 @@ import UIKit
 
 class ViewController: UITableViewController {
 
+    // MARK: - Model
+    
+    var items: [String] = []
+
+    var fetchInProgress = false
+
+    func fetchMoreData() {
+        if fetchInProgress {
+            return
+        } else {
+            fetchInProgress = true
+            
+            // This simulates a background fetch by delaying for a second
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1), dispatch_get_main_queue(), {
+                self.items += self.generateSomeItems()
+                
+                self.tableView.beginUpdates()
+                self.tableView.insertRowsAtIndexPaths([NSIndexPath(forRow: self.items.count, inSection: 0)], withRowAnimation: .Top)
+                self.fetchInProgress = false
+                self.tableView.endUpdates()
+            })
+        }
+    }
+    
+    private func generateSomeItems() -> [String] {
+        let formatter = NSNumberFormatter()
+        
+        formatter.numberStyle = .SpellOutStyle
+        
+        var generatedItems: [String] = []
+        
+        for _ in 0...20 {
+            generatedItems.append(formatter.stringFromNumber(items.count + 1)!)
+        }
+        
+        return generatedItems
+    }
+
+
+    // MARK: - View lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        items = generateSomeItems()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: - Table view stuff
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count + 1
     }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        if indexPath.row < self.items.count {
+            let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+            
+            cell.textLabel!.text = items[indexPath.row]
+            
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCellWithIdentifier("Loading", forIndexPath: indexPath) as? LoadingTableViewCell
 
+//            cell.activityIndicatorView.startAnimating()
+            
+            fetchMoreData()
+            
+            return cell!
+        }
+    }
 
 }
 
