3bd11abad2665dad68383b0bd60a8872ca82156e
wvdk <wes.vdk@icloud.com>
2016-04-24 10:44:19 -0700

    Init another coredata follow-along

---
+++ Learning CoreData/Modeling relationships/Modeling relationships/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Modeling relationships
+//
+//  Created by Wes Van der Klomp on 4/24/16.
+//  Copyright © 2016 wvdk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Learning CoreData/Modeling relationships/Modeling relationships/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Modeling relationships
+//
+//  Created by Wes Van der Klomp on 4/24/16.
+//  Copyright © 2016 wvdk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
