4cd4f400baf5aa2e5df96270347f831442cf0b3f
Wesley Van der Klomp <wes.vdk@icloud.com>
2016-09-27 18:06:39 -0700

    create Learning Swift and Cocoa

--- Quick-access Playgrounds for Dock/macOS.playground/Contents.swift
+++ Quick-access Playgrounds for Dock/macOS.playground/Contents.swift
@@ -1,35 +1,17 @@
-import Cocoa        // (or UIKit for iOS)
-import SceneKit
-import QuartzCore   // for the basic animation
+import Cocoa
 
-// create a scene view with an empty scene
-var sceneView = SCNView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
-var scene = SCNScene()
-sceneView.scene = scene
+print("Hi")
 
-// default lighting
-sceneView.autoenablesDefaultLighting = true
 
-// a camera
-var cameraNode = SCNNode()
-cameraNode.camera = SCNCamera()
-cameraNode.position = SCNVector3(x: 0, y: 0, z: 3)
-scene.rootNode.addChildNode(cameraNode)
-
-// a geometry object
-var torus = SCNTorus(ringRadius: 1, pipeRadius: 0.35)
-var torusNode = SCNNode(geometry: torus)
-scene.rootNode.addChildNode(torusNode)
-
-// configure the geometry object
-torus.firstMaterial?.diffuse.contents  = NSColor.redColor()   // (or UIColor on iOS)
-torus.firstMaterial?.specular.contents = NSColor.whiteColor() // (or UIColor on iOS)
-
-// set a rotation axis (no angle) to be able to
-// use a nicer keypath below and avoid needing
-// to wrap it in an NSValue
-//torusNode.rotation = SCNVector4(x: 1.0, y: 1.0, z: 0.0, w: 0.0)
-
-scene
-
+class ActionTrampoline: NSObject {
+    var action: (NSControl) -> Void
+    
+    init(action: @escaping (NSControl) -> Void) {
+        self.action = action
+    }
+    
+    @objc func action(sender: NSControl) {
+        action(sender)
+    }
+}
 
