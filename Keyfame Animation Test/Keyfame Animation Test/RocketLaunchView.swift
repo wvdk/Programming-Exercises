0f8e0a53b1e86bd1e87e4de16508796de911fc17
Wesley Van der Klomp <wes.vdk@icloud.com>
2017-02-05 12:35:59 -0800

    Use paintcode to make rocket and add stylekit to project

--- Keyfame Animation Test/Keyfame Animation Test/RocketLaunchView.swift
+++ Keyfame Animation Test/Keyfame Animation Test/RocketLaunchView.swift
@@ -13,19 +13,22 @@ class RocketLaunchView: UIView {
     let planetLayer = CALayer()
     
     private func setupPlanetLayer() {
-        planetLayer.position = CGPoint(x: 0, y: 0)
-        planetLayer.backgroundColor = UIColor(red: 0.0, green: 180.0, blue: 100.0, alpha: 1.0).cgColor
-        
         let heightOfContainingView = self.frame.size.height
+
+        planetLayer.position = CGPoint(x: -heightOfContainingView / 1.5, y: 0)
+        planetLayer.backgroundColor = UIColor(red: 0.2, green: 0.9, blue: 0.5, alpha: 0.8).cgColor
+        
         planetLayer.bounds = CGRect(x: 0, y: 0, width: heightOfContainingView, height: heightOfContainingView)
         
         planetLayer.cornerRadius = heightOfContainingView / 2
         planetLayer.anchorPoint = CGPoint(x: 0, y: 0)
     }
-
+    
     override func draw(_ rect: CGRect) {
         setupPlanetLayer()
         self.layer.addSublayer(planetLayer)
+        
+        self.clipsToBounds = true
     }
 
 }
