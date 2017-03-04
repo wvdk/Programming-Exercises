c5e0c11a0c03f6e46f7c472f90cd0b1107bc347c
Wes Van der Klomp <wes.vdk@icloud.com>
2017-03-04 11:17:51 -0800

    add firebase test

--- Firebase/HelloWorld/HelloWorld/ViewController.swift
+++ Firebase/HelloWorld/HelloWorld/ViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    @IBAction func signIn(_ sender: Any) {
+        FIRAuth.auth()?.signIn(withEmail: emailField.text!, password: passwordField.text!, completion: { (user, error) in
+            if error == nil {
+                self.view.backgroundColor = .green
+            }
+        })
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
--- Playgrounds/MyPlayground.playground/Contents.swift
+++ Playgrounds/MyPlayground.playground/Contents.swift
@@ -1,34 +1,34 @@
-import UIKit
-import PlaygroundSupport
-
-///////////////////////////////////
-
-for x in 0...10 {
-    var y = x * 10
-}
-
-sqrt(2)
-
-
-
-////////////////////////////////////
-
-let mainView = DialView(frame: CGRect(x: 0, y: 0, width: 243, height: 243/2))
-
-mainView.backgroundColor = UIColor.white
-
-PlaygroundPage.current.liveView = mainView
-
-let dialObj = DialView()
-
-
-////////////////////////////////////////
-
-
-let myArray: [Any] = [1, "Array", false]
-
-myArray.first
-
+//import UIKit
+//import PlaygroundSupport
+//
+/////////////////////////////////////
+//
+//for x in 0...10 {
+//    var y = x * 10
+//}
+//
+//sqrt(2)
+//
+//
+//
+//////////////////////////////////////
+//
+//let mainView = DialView(frame: CGRect(x: 0, y: 0, width: 243, height: 243/2))
+//
+//mainView.backgroundColor = UIColor.white
+//
+//PlaygroundPage.current.liveView = mainView
+//
+//let dialObj = DialView()
+//
+//
+//////////////////////////////////////////
+//
+//
+//let myArray: [Any] = [1, "Array", false]
+//
+//myArray.first
+//
 
 
 
@@ -38,4 +38,12 @@ myArray.first
 
 // I want to understand the pitfalls of referance loops everyone is always talking about.
 
+
+///////////////////
+
+var a = UInt32.max
+print(a)
+
+var b = UInt64.max
+print(b)
 
