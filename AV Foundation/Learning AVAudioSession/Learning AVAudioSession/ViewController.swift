27dfde34512202ce549019c39471e35a5823efe2
Wes Van der Klomp <wes.vdk@icloud.com>
2017-05-01 12:31:10 -0700

    oops, add project

---
+++ AV Foundation/Learning AVAudioSession/Learning AVAudioSession/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Learning AVAudioSession
+//
+//  Created by Wesley Van der Klomp on 5/1/17.
+//  Copyright © 2017 wvdk. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
