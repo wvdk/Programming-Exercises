a052fac51f3ff29cc54c9321d45286bf1589b989
Wes Van der Klomp <wes.vdk@icloud.com>
2017-05-05 14:31:47 -0700

    create login controller

---
+++ Firebase/gameofchats/gameofchats/LoginController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginController.swift
+//  gameofchats
+//
+//  Created by Wesley Van der Klomp on 5/5/17.
+//  Copyright © 2017 wvdk. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
+
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+}
--- Firebase/gameofchats/gameofchats/ViewController.swift
+++ Firebase/gameofchats/gameofchats/ViewController.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
+    }
+    
+    func handleLogout() {
+        let loginController = LoginController()
+        present(loginController, animated: true, completion: nil)
     }
 
 }
