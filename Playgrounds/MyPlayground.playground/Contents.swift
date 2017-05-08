4470fdbe3d415e1d1224ee82348aeb0a6dfe7593
Wes Van der Klomp <wes.vdk@icloud.com>
2017-05-08 11:01:03 -0700

    test string comparison

--- Playgrounds/MyPlayground.playground/Contents.swift
+++ Playgrounds/MyPlayground.playground/Contents.swift
@@ -1,48 +1,9 @@
 
-enum MyError: Error {
-    case fuck
-}
+let myString1 = "Test"
+let myString2 = "test"
 
-func throwingFunction(shouldThrow: Bool) throws -> Bool {
-    if shouldThrow {
-        throw MyError.fuck
-    } else {
-        return true
-    }
-}
 
-do {
-    try throwingFunction(shouldThrow: true)
-} catch {
-    print(error)
-}
-
-let myInt = 1
-
-let myInt2: (Int) = 1
-
-myInt == myInt2
+myString1.lowercased() == myString2
 
 
 
-
-class IntegerRef: Equatable {
-    let value: Int
-    init(_ value: Int) {
-        self.value = value
-    }
-    
-    static func ==(lhs: IntegerRef, rhs: IntegerRef) -> Bool {
-        return lhs.value == rhs.value
-    }
-}
-
-
-let i1 = IntegerRef(34)
-let i2 = i1
-let i3 = IntegerRef(34)
-
-i1 === i2
-i1 == i2
-i3 == i1
-i3 === i1
