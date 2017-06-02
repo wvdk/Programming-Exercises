5c278db5ff9e1feba7bead7807cd06b498ae8147
Wes Van der Klomp <wes.vdk@icloud.com>
2017-06-01 17:10:38 -0700

    init project

---
+++ macOS/Testing Document Based App/Testing Document Based App/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Testing Document Based App
+//
+//  Created by Wesley Van der Klomp on 6/1/17.
+//  Copyright © 2017 Wesley Van der Klomp. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ macOS/Testing Document Based App/Testing Document Based App/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Testing Document Based App
+//
+//  Created by Wesley Van der Klomp on 6/1/17.
+//  Copyright © 2017 Wesley Van der Klomp. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
