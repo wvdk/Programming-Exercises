df1dd79ac80d2f2ca131bc0f4fe865f751e29f98
Wes Van der Klomp <wes.vdk@icloud.com>
2017-06-06 21:15:14 -0700

    init piano project

---
+++ SceneKit/Piano/Piano/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Piano
+//
+//  Created by Wesley Van der Klomp on 6/6/17.
+//  Copyright © 2017 Wesley Van der Klomp. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
