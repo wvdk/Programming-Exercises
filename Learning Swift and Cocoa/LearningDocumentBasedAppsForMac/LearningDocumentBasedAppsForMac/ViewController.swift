2398692577f56e95a30f22aa06e1517610befcba
Wes Van der Klomp <wes.vdk@icloud.com>
2017-06-30 12:20:16 +0300

    document based app for mac

---
+++ Learning Swift and Cocoa/LearningDocumentBasedAppsForMac/LearningDocumentBasedAppsForMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LearningDocumentBasedAppsForMac
+//
+//  Created by Wesley Van der Klomp on 6/30/17.
+//  Copyright © 2017 Wesley Van der Klomp. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
