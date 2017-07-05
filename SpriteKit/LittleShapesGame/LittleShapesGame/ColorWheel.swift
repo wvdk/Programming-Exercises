a6c7d325a3de92a2851d16303198515d73f74051
Wes Van der Klomp <wes.vdk@icloud.com>
2017-07-05 16:05:37 +0300

    create path for quarter circle

--- SpriteKit/LittleShapesGame/LittleShapesGame/ColorWheel.swift
+++ SpriteKit/LittleShapesGame/LittleShapesGame/ColorWheel.swift
@@ -12,6 +12,23 @@ import SpriteKit
 class ColorWheel: SKNode {
     override init() {
         super.init()
+        
+        let path = NSBezierPath()
+        path.move(to: NSPoint(x: 0, y: -200))
+        path.line(to: NSPoint(x: 0, y: -160))
+        path.appendArc(withCenter: NSPoint.zero,
+                       radius: 160.0,
+                       startAngle: CGFloat(3.0 * M_PI_2),
+                       endAngle: CGFloat(0),
+                       clockwise: true)
+        path.line(to: NSPoint(x: 200, y: 0))
+        path.appendArc(withCenter: NSPoint.zero,
+                       radius: 200,
+                       startAngle: CGFloat(3.0),
+                       endAngle: CGFloat(3.0 * M_PI_2),
+                       clockwise: false)
+        
+        let bluePart = SKShapeNode(path: path)
     }
     
     required init?(coder aDecoder: NSCoder) {
