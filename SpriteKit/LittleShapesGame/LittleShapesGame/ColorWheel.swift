6a5093970fbb2dd4025bf166b7ccf3fa58f44d3e
Wes Van der Klomp <wes.vdk@icloud.com>
2017-07-05 12:08:29 +0300

    create ColorWheel class

---
+++ SpriteKit/LittleShapesGame/LittleShapesGame/ColorWheel.swift
@@ -0,0 +1,20 @@
+//
+//  ColorWheel.swift
+//  LittleShapesGame
+//
+//  Created by Wesley Van der Klomp on 7/5/17.
+//  Copyright © 2017 Wesley Van der Klomp. All rights reserved.
+//
+
+import Cocoa
+import SpriteKit
+
+class ColorWheel: SKNode {
+    override init() {
+        super.init()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
