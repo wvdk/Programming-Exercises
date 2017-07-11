77f800b080d2f738824a573a1cf5450f52ba8889
Wes Van der Klomp <wes.vdk@icloud.com>
2017-07-11 13:35:54 +0300

    add button scene and class

---
+++ SpriteKit/TestingReferenceNodes/TestingReferenceNodes/Button.swift
@@ -0,0 +1,13 @@
+//
+//  Button.swift
+//  TestingReferenceNodes
+//
+//  Created by Wesley Van der Klomp on 7/11/17.
+//  Copyright © 2017 Wesley Van der Klomp. All rights reserved.
+//
+
+import Cocoa
+
+class Button: SKSpriteNode {
+
+}
