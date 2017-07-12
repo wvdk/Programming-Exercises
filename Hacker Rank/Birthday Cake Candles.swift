2aca7cd5ee10259ddc579fac08a561e3c6376651
Wes Van der Klomp <wes.vdk@icloud.com>
2017-07-12 13:51:29 +0300

    create command line version

---
+++ Hacker Rank/Birthday Cake Candles.swift
@@ -0,0 +1,34 @@
+//
+//  Birthday Cake Candles.swift
+//  
+//
+//  Created by Wesley Van der Klomp on 7/12/17.
+//
+
+import Foundation
+
+// number of elements
+var n = Int(readLine()!)!
+
+// read array and map the elements to integer
+var arr = readLine()!.characters.split(separator: " ", maxSplits: 100, omittingEmptySubsequences: false).map{Int(String($0))!}
+
+////////////////////////
+
+func numberOfTallestCandles(_ candles: [Int]) -> Int {
+    var heighestFound = 0
+    var numberOfHeighestsFound = 0
+    
+    for candleHeight in candles {
+        if candleHeight > heighestFound {
+            heighestFound = candleHeight
+            numberOfHeighestsFound = 1
+        } else if candleHeight == heighestFound {
+            numberOfHeighestsFound += 1
+        }
+    }
+    
+    return numberOfHeighestsFound
+}
+
+print(numberOfTallestCandles(arr))
