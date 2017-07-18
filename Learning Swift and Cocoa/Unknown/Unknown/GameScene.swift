a5d3bebee7c3353943b2a50f48d07a1d03c77027
Wes Van der Klomp <wes.vdk@icloud.com>
2017-07-18 12:00:49 +0300

    more cleanup

--- Learning Swift and Cocoa/Unknown/Unknown/GameScene.swift
+++ Learning Swift and Cocoa/Unknown/Unknown/GameScene.swift
@@ -11,55 +11,23 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var player : SKSpriteNode?
+    private var player: SKSpriteNode?
     
     override func didMove(to view: SKView) {
         player = childNode(withName: "//Player") as? SKSpriteNode
     }
     
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
-    
-    func touchDown(atPoint pos: CGPoint) {
-        print("Touch down")
-        
+    override func mouseDown(with event: NSEvent) {
         if let player = player {
             player.run(SKAction(named: "Pulse")!)
-            player.position = pos
+            player.position = event.location(in: self)
         }
     }
     
-    func touchMoved(toPoint pos : CGPoint) {
-        
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        
-    }
-    
-    override func mouseDown(with event: NSEvent) {
-        self.touchDown(atPoint: event.location(in: self))
-    }
-    
     override func mouseDragged(with event: NSEvent) {
-        self.touchMoved(toPoint: event.location(in: self))
+        
     }
     
     override func mouseUp(with event: NSEvent) {
-        self.touchUp(atPoint: event.location(in: self))
     }
-    
-//    override func keyDown(with event: NSEvent) {
-//        switch event.keyCode {
-//        case 0x31:
-//            if let label = self.label {
-//                label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-//            }
-//        default:
-//            print("keyDown: \(event.characters!) keyCode: \(event.keyCode)")
-//        }
-//    }
-    
-    
 }
