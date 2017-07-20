30a9b9b904bc25b2e08aae20e637b1e970738147
Wes Van der Klomp <wes.vdk@icloud.com>
2017-07-20 09:57:55 +0300

    set up testing

--- Hacker Rank/Compare the Triplets.playground/Contents.swift
+++ Hacker Rank/Compare the Triplets.playground/Contents.swift
@@ -4,11 +4,15 @@ func compareTriplets() {
     
 }
 
-func parseAndCompareTriplets(rawInput: String) -> String {
+func parseThenCompareTriplets(rawInput: String) -> String {
     return ""
 }
 
 let sampleInput = "5 6 7\n3 6 10"
 let sampleOutput = "1 1"
 
-if parseAndCompareTriplets()
+if parseThenCompareTriplets(rawInput: sampleInput) == sampleOutput {
+    print("Passed test")
+} else {
+    print("Failed test")
+}
