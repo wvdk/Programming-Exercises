a8f03ede91a66a6c08c74f92e6fe2bc4eed1ef22
Wes Van der Klomp <wes.vdk@icloud.com>
2017-07-20 11:12:35 +0300

    break things up into smaller functions

--- Hacker Rank/Compare the Triplets.playground/Contents.swift
+++ Hacker Rank/Compare the Triplets.playground/Contents.swift
@@ -1,34 +1,31 @@
 import Cocoa
 
+let sampleInput = "5 6 7\n3 6 10"
+let sampleOutput = "1 1"
 
 typealias Triplet = (Int, Int, Int)
+typealias Score = (Int, Int)
 
-func compareTriplets(a: Triplet, b: Triplet) -> (aScore: Int, bScore: Int){
+func compareTriplets(a: Triplet, b: Triplet) -> Score {
     return (1, 1)
 }
 
-func parseThenCompareTriplets(rawInput: String) -> String {
-    
+func parse(rawInput: String) -> (a: Triplet, b: Triplet) {
     let bothLines = rawInput.split(separator: "\n")
     let lineA = bothLines[0].split(separator: " ").map { Int(String($0))! }
     let lineB = bothLines[1].split(separator: " ").map { Int(String($0))! }
     let tripletA: Triplet = (lineA[0], lineA[1], lineA[2])
     let tripletB: Triplet = (lineB[0], lineB[1], lineB[2])
-
-    let scores = compareTriplets(a: tripletA, b: tripletB)
     
-    return "\(scores.aScore) \(scores.bScore)"
+    return (a: tripletA, b: tripletB)
 }
 
-
-// MARK: Testing
-
-let sampleInput = "5 6 7\n3 6 10"
-let sampleOutput = "1 1"
-
-if parseThenCompareTriplets(rawInput: sampleInput) == sampleOutput {
-    print("Passed test")
-} else {
-    print("Failed test")
+func output(result: Score) {
+    print("\(result.0) \(result.1)")
 }
+
+let parsedInput = parse(rawInput: sampleInput)
+let score = compareTriplets(a: parsedInput.a, b: parsedInput.b)
+
+output(result: score)
 
