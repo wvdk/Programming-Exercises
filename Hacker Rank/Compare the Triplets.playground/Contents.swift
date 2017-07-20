0c0e8277d4a73a0b42deb18a16e9b7df518dc0e2
Wes Van der Klomp <wes.vdk@icloud.com>
2017-07-20 10:59:57 +0300

    get parsing working

--- Hacker Rank/Compare the Triplets.playground/Contents.swift
+++ Hacker Rank/Compare the Triplets.playground/Contents.swift
@@ -1,14 +1,27 @@
 import Cocoa
 
+
 typealias Triplet = (Int, Int, Int)
 
 func compareTriplets(a: Triplet, b: Triplet) -> (aScore: Int, bScore: Int){
-    return (0, 0)
+    return (1, 1)
 }
 
 func parseThenCompareTriplets(rawInput: String) -> String {
-    return ""
+    
+    let bothLines = rawInput.split(separator: "\n")
+    let lineA = bothLines[0].split(separator: " ").map { Int(String($0))! }
+    let lineB = bothLines[1].split(separator: " ").map { Int(String($0))! }
+    let tripletA: Triplet = (lineA[0], lineA[1], lineA[2])
+    let tripletB: Triplet = (lineB[0], lineB[1], lineB[2])
+
+    let scores = compareTriplets(a: tripletA, b: tripletB)
+    
+    return "\(scores.aScore) \(scores.bScore)"
 }
+
+
+// MARK: Testing
 
 let sampleInput = "5 6 7\n3 6 10"
 let sampleOutput = "1 1"
@@ -18,3 +31,4 @@ if parseThenCompareTriplets(rawInput: sampleInput) == sampleOutput {
 } else {
     print("Failed test")
 }
+
