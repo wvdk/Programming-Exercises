e0b17a8d4bbffc37d7bdcbca237908fc609a759b
Wes Van der Klomp <wes.vdk@icloud.com>
2017-07-21 10:08:06 +0300

    alt version

--- Hacker Rank/Kangaroo.playground/Contents.swift
+++ Hacker Rank/Kangaroo.playground/Contents.swift
@@ -25,6 +25,19 @@ func doesIntersect(kangarooA: Kangaroo, kangarooB: Kangaroo) -> Bool {
     return false
 }
 
+func doesIntersectAlt(kangarooA: Kangaroo, kangarooB: Kangaroo) -> Bool {
+    let xDiff = kangarooA.x - kangarooB.x
+    let vDiff = kangarooB.v - kangarooA.v
+    let div = xDiff / vDiff
+    let remainder = xDiff % vDiff
+    
+    if remainder == 0 && div >= 0 {
+        return true
+    } else {
+        return false
+    }
+}
+
 func parse(input: String) -> (kangarooA: Kangaroo, kangarooB: Kangaroo) {
     let a = input.split(separator: " ").map { Int(String($0))! }
     
@@ -38,12 +51,14 @@ let sampleOutput0 = "YES"
 
 let parsedInput0 = parse(input: sampleInput0)
 doesIntersect(kangarooA: parsedInput0.kangarooA, kangarooB: parsedInput0.kangarooB)
+doesIntersectAlt(kangarooA: parsedInput0.kangarooA, kangarooB: parsedInput0.kangarooB)
 
 
 let sampleInput1 = "0 2 5 3"
 let sampleOutput1 = "NO"
 
 let parsedInput1 = parse(input: sampleInput1)
-//doesIntersect(kangarooA: parsedInput1.kangarooA, kangarooB: parsedInput1.kangarooB)
+doesIntersect(kangarooA: parsedInput1.kangarooA, kangarooB: parsedInput1.kangarooB)
+doesIntersectAlt(kangarooA: parsedInput1.kangarooA, kangarooB: parsedInput1.kangarooB)
 
 
