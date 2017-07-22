23d19f2714c4e689625a29a5011a559fa53eb398
Wes Van der Klomp <wes.vdk@icloud.com>
2017-07-22 08:34:03 +0300

    basically working except zeros

--- Hacker Rank/Time Conversion.playground/Contents.swift
+++ Hacker Rank/Time Conversion.playground/Contents.swift
@@ -1,4 +1,26 @@
-//:https://www.hackerrank.com/challenges/time-conversion
+//: https://www.hackerrank.com/challenges/time-conversion
 
 import Cocoa
 
+func convertTo24HourTime(input: String) -> String {
+    
+    let a = input.split(separator: ":")
+    var hours = Int(String(a[0]))!
+    let minutes = Int(String(a[1]))!
+    let secondsWithAmPm = String(a[2])
+    let index = secondsWithAmPm.index(secondsWithAmPm.startIndex, offsetBy: 2)
+    var amPm = secondsWithAmPm.substring(from: index)
+    let seconds = Int(String(secondsWithAmPm.substring(to: index)))!
+    
+    if amPm == "PM" {
+        hours += 12
+    }
+    
+    return "\(hours):\(minutes):\(seconds)"
+}
+
+
+let sampleInput = "07:05:45PM"
+let sampleOutput = "19:05:45"
+
+sampleOutput == convertTo24HourTime(input: sampleInput)
