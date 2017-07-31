747ce9411410d6c9d31a627bf68322dff83623f5
Wes Van der Klomp <wes.vdk@icloud.com>
2017-07-31 08:57:45 +0300

    what I have so far

--- Hacker Rank/Index of Value/Index of Value/main.swift
+++ Hacker Rank/Index of Value/Index of Value/main.swift
@@ -5,8 +5,17 @@
 //  Created by Wesley Van der Klomp on 7/31/17.
 //  Copyright © 2017 Wesley Van der Klomp. All rights reserved.
 //
+//  https://www.hackerrank.com/challenges/tutorial-intro
+//
 
 import Foundation
 
-print("Hello, World!")
+let v = readLine()!
+let n = readLine()!
+let ar = readLine()!
 
+let arAsArray = ar.characters.split(separator: " ").map { Int(String($0))! }
+
+print(ar)
+print(arAsArray)
+print(arAsArray.index(of: Int(v)!))
