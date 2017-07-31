1114cfda7ccb67d1bab4158271856e400ce9c3c5
Wes Van der Klomp <wes.vdk@icloud.com>
2017-07-31 09:03:53 +0300

    add sample input/output comment and create vAsInt

--- Hacker Rank/Index of Value/Index of Value/main.swift
+++ Hacker Rank/Index of Value/Index of Value/main.swift
@@ -8,14 +8,33 @@
 //  https://www.hackerrank.com/challenges/tutorial-intro
 //
 
+/*
+ 
+ Sample Input:
+ 
+ 4
+ 6
+ 1 4 5 7 9 12
+ 
+ Sample Output:
+ 
+ 1
+ 
+ */
+
+
 import Foundation
 
 let v = readLine()!
 let n = readLine()!
 let ar = readLine()!
-
+v
+let vAsInt = Int(v)!
 let arAsArray = ar.characters.split(separator: " ").map { Int(String($0))! }
 
-print(ar)
-print(arAsArray)
-print(arAsArray.index(of: Int(v)!))
+let indexOfV = arAsArray.index(of: vAsInt)?.description
+
+print(indexOfV)
+
+//print(arAsArray.index(of: Int(v)!))
+
