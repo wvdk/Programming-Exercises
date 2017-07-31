2d7a3a62a653d12485d206177e9ea0e5e24bc611
Wes Van der Klomp <wes.vdk@icloud.com>
2017-07-31 11:05:28 +0300

    init project

---
+++ Hacker Rank/Big Sort/Big Sort/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Big Sort
+//
+//  Created by Wesley Van der Klomp on 7/31/17.
+//  Copyright © 2017 Wesley Van der Klomp. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
