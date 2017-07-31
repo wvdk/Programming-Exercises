c377df00a146eeac632a7e0f7d9fa6a20a8fcf35
Wes Van der Klomp <wes.vdk@icloud.com>
2017-07-31 11:14:43 +0300

    for loop working to read lines

--- Hacker Rank/Big Sort/Big Sort/main.swift
+++ Hacker Rank/Big Sort/Big Sort/main.swift
@@ -5,8 +5,35 @@
 //  Created by Wesley Van der Klomp on 7/31/17.
 //  Copyright © 2017 Wesley Van der Klomp. All rights reserved.
 //
+// https://www.hackerrank.com/challenges/big-sorting
+//
+
+/*
+ Sample Input 0
+ 
+ 6
+ 31415926535897932384626433832795
+ 1
+ 3
+ 10
+ 3
+ 5
+ 
+ Sample Output 0
+ 
+ 1
+ 3
+ 3
+ 5
+ 10
+ 31415926535897932384626433832795
+ */
 
 import Foundation
 
-print("Hello, World!")
+let n = Int(readLine()!.trimmingCharacters(in: .whitespacesAndNewlines))!
+
+for _ in 0...n {
+    print(readLine()!)
+}
 
