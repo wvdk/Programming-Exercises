f91a60e72798dd2d8c61bc6834c3a1f3c8d244a0
Wes Van der Klomp <wes.vdk@icloud.com>
2017-08-02 10:43:37 +0300

    add line

--- Playgrounds/Graphics.playground/Contents.swift
+++ Playgrounds/Graphics.playground/Contents.swift
@@ -7,14 +7,23 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
 
+        let line = SKShapeNode(rect: CGRect(x: 10, y: 10, width: 10, height: 600))
+        
+        line.fillColor = .red
+        line.strokeColor = .red
+        
+        addChild(line)
+        
+        line.position = CGPoint(x: 10, y: 10)
     }
     
 }
 
-let sceneView = SKView(frame: CGRect(x:0 , y:0, width: 400, height: 400))
+let sceneView = SKView(frame: CGRect(x:0 , y:0, width: 600, height: 600))
 let scene = GameScene()
 
 scene.scaleMode = .aspectFill
+scene.backgroundColor = .white
 sceneView.presentScene(scene)
 
 PlaygroundSupport.PlaygroundPage.current.liveView = sceneView
