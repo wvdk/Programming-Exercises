dea4f2567bde5bba4ae00b28b94a4aabbf922650
Wes Van der Klomp <wes.vdk@icloud.com>
2017-08-03 15:40:24 +0300

    move touch stuff to basic UIResponder methods

--- SpriteKit/DragLeft/DragLeft/GameScene.swift
+++ SpriteKit/DragLeft/DragLeft/GameScene.swift
@@ -20,43 +20,38 @@ class GameScene: SKScene {
 
     override func didMove(to view: SKView) {
 //        topLeftPoint = CGPoint(x: ((view.frame.width / 2) * -1) + 30, y: ((view.frame.height / 2) * -1) + 30)
-        
-        rectNode.lineWidth = 10
-        rectNode.anchorPoint = CGPoint(x: 0.0, y: 0.0)
     }
-    
-    func touchDown(atPoint pos : CGPoint) {
-        addChild(rectNode)
-        
-        let rect = CGRect(x: Int(topLeftPoint.x), y: Int(topLeftPoint.y), width: Int(pos.x), height: Int(pos.y))
 
-        rectNode.position = topLeftPoint
-        rectNode.path = CGPath(rect: rect, transform: nil)
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        let rect = CGRect(x: Int(topLeftPoint.x), y: Int(topLeftPoint.y), width: Int(pos.x), height: Int(pos.y))
-        
-        rectNode.path = CGPath(rect: rect, transform: nil)
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        rectNode.removeFromParent()
-    }
+    // MARK: - Handle touches
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        for touch in touches {
+            let pos = touch.previousLocation(in: self)
+            
+//            let rect = CGRect(x: Int(topLeftPoint.x), y: Int(topLeftPoint.y), width: Int(pos.x), height: Int(pos.y))
+            
+//            rectNode.position = topLeftPoint
+//            rectNode.path = CGPath(rect: rect, transform: nil)
+            
+            addChild(rectNode)
+        }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        for touch in touches {
+//            let
+//            let rect = CGRect(x: Int(topLeftPoint.x), y: Int(topLeftPoint.y), width: Int(pos.x), height: Int(pos.y))
+//
+//            rectNode.path = CGPath(rect: rect, transform: nil)
+        
+        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+
     }
 }
