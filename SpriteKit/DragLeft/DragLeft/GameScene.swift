77896c6e9e0c644084cb157dcb8efd861f769c26
Wes Van der Klomp <wes.vdk@icloud.com>
2017-08-03 10:35:48 +0300

    remove label

--- SpriteKit/DragLeft/DragLeft/GameScene.swift
+++ SpriteKit/DragLeft/DragLeft/GameScene.swift
@@ -11,7 +11,6 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
     @objc func sliding(gesture: UIGestureRecognizer) {
@@ -20,18 +19,9 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         
-        let gesture = UIGestureRecognizer(target: self, action: #selector(sliding(gesture:)))
+//        let gesture = UIGestureRecognizer(target: self, action: #selector(sliding(gesture:)))
         
-            
-            
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
         
-        // Create shape node to use during mouse interaction
         let w = (self.size.width + self.size.height) * 0.05
         self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
         
@@ -42,7 +32,7 @@ class GameScene: SKScene {
             spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
                                               SKAction.fadeOut(withDuration: 0.5),
                                               SKAction.removeFromParent()]))
-            spinnyNode.run(SKAction(named: "Grow")!)
+//            spinnyNode.run(SKAction(named: "Grow")!)
         }
     }
     
@@ -72,10 +62,6 @@ class GameScene: SKScene {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
@@ -89,10 +75,5 @@ class GameScene: SKScene {
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
         for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
     }
 }
