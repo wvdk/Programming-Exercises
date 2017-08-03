aa9c8b551186fc71ba8d96ca39aa2260ef23925d
Wes Van der Klomp <wes.vdk@icloud.com>
2017-08-03 15:26:24 +0300

    add more documentation comments

--- SpriteKit/DragLeft/DragLeft/RectNode.swift
+++ SpriteKit/DragLeft/DragLeft/RectNode.swift
@@ -13,13 +13,11 @@ class RectNode: SKShapeNode {
     
     /// Defines the point in the node's frame that should act as the center point for the node’s position.
     ///
-    /// A value of (0.0, 0.0) is the top left and (1.0, 1.0) is bottom right. Defaults to 0.5, 0.5 which dead center. Set anchorPoint, then set the position property to see effects. Works much like SKSpriteNode's implementation.
+    /// A value of (0.0, 0.0) is the top left and (1.0, 1.0) is bottom right. Defaults to 0.5, 0.5 which dead center. Set `anchorPoint`, then set the `position` property to see effects. Works much like `SKSpriteNode`'s implementation, so see "Using the Anchor Point to Move the Sprite Node’s Frame" in `SKSpriteNode` documentation.
     var anchorPoint = CGPoint(x: 0.5, y: 0.5)
     
     /// Creates an instance with line width and stoke color set to reasonable defaults.
     override init() {
-
-        
         super.init()
         
         lineWidth = 8.0
