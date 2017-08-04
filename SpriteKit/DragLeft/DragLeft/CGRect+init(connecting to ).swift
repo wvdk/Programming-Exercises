ad38f9818e4d892460f76314fb5b01fe2d4632f0
Wes Van der Klomp <wes.vdk@icloud.com>
2017-08-04 10:26:26 +0300

    do same as width for height

--- SpriteKit/DragLeft/DragLeft/CGRect+init(connecting to ).swift
+++ SpriteKit/DragLeft/DragLeft/CGRect+init(connecting to ).swift
@@ -13,7 +13,7 @@ extension CGRect {
     /// Creates an instance of a rectangle with two of it's corners located at the a and b points.
     init(connecting pointA: CGPoint, to pointB: CGPoint) {
         let width =  pointA.x - pointB.x
-        let height = CGFloat(100)
+        let height = pointB.x - pointB.x
         
         self.init(x: pointA.x, y: pointA.y, width: width, height: height)
     }
