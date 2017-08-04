2a1ccb84800bee16fc056d34b96de155178c2c25
Wes Van der Klomp <wes.vdk@icloud.com>
2017-08-04 10:02:42 +0300

    create CGRect extension file

---
+++ SpriteKit/DragLeft/DragLeft/CGRect+init(connecting to ).swift
@@ -0,0 +1,10 @@
+//
+//  CGRect+init(connecting to ).swift
+//  DragLeft
+//
+//  Created by Wesley Van der Klomp on 8/4/17.
+//  Copyright © 2017 Wesley Van der Klomp. All rights reserved.
+//
+
+import Foundation
+
