ff7aad3e61a8ff41cd159d7ffa0b8a02e91540e0
Wes Van der Klomp <wes.vdk@icloud.com>
2017-08-04 13:57:48 +0300

    update to match https://github.com/wvdk/A-Swift-Vocabulary version

--- SpriteKit/DragLeft/DragLeft/CGRect+init(cornersSpanning).swift
+++ SpriteKit/DragLeft/DragLeft/CGRect+init(cornersSpanning).swift
@@ -1,16 +1,15 @@
 //
 //  CGRect+init(cornersSpanning).swift
-//  DragLeft
+//  https://github.com/wvdk/A-Swift-Vocabulary
 //
 //  Created by Wesley Van der Klomp on 8/4/17.
-//  Copyright © 2017 Wesley Van der Klomp. All rights reserved.
 //
 
 import CoreGraphics
 
 extension CGRect {
     
-    /// Creates a rectangle with corners reaching from `pointA` to `pointB`.
+    /// Creates a rectangle with corners reaching from one point to another.
     init(cornersSpanning from: CGPoint, to: CGPoint) {
         let width =  to.x - from.x
         let height =  to.y - from.y
@@ -19,3 +18,4 @@ extension CGRect {
     }
     
 }
+
