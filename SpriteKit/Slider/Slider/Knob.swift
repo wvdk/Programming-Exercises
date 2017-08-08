c4905cd68175c3bdae84bf1f2fb9965b553e6ad1
Wes Van der Klomp <wes.vdk@icloud.com>
2017-08-08 16:47:50 +0300

    create Knob struct with node and position properties

---
+++ SpriteKit/Slider/Slider/Knob.swift
@@ -0,0 +1,26 @@
+//
+//  Knob.swift
+//  Slider
+//
+//  Created by Wesley Van der Klomp on 8/8/17.
+//  Copyright © 2017 Wesley Van der Klomp. All rights reserved.
+//
+
+import Cocoa
+import SpriteKit
+
+/// The part of a slider than the user clicks on and drags in order to change the value.
+struct Knob {
+    
+    /// An SKShapeNode for adding to a SpriteKit scene.
+    var node: SKShapeNode {
+        let n = SKShapeNode(circleOfRadius: 30)
+        
+        return n
+    }
+    
+    /// The position of the knob in it's parents coordinate system. Also used to calculate the slider's value.
+    var position: CGPoint
+    
+    
+}
