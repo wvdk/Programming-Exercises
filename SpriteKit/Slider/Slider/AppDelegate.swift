9debe8b76c7fc70601adebe77d65b39b48c50705
Wes Van der Klomp <wes.vdk@icloud.com>
2017-08-08 16:25:07 +0300

    init project

---
+++ SpriteKit/Slider/Slider/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Slider
+//
+//  Created by Wesley Van der Klomp on 8/8/17.
+//  Copyright © 2017 Wesley Van der Klomp. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
