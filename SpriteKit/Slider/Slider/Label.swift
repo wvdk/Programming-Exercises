d57c7d2a2b90fa2a03c362004a9296f00769218e
Wes Van der Klomp <wes.vdk@icloud.com>
2017-08-08 17:03:57 +0300

    create Label

---
+++ SpriteKit/Slider/Slider/Label.swift
@@ -0,0 +1,14 @@
+//
+//  Label.swift
+//  Slider
+//
+//  Created by Wesley Van der Klomp on 8/8/17.
+//  Copyright © 2017 Wesley Van der Klomp. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+struct Label {
+    
+}
