ed9970ed51ebd477af14fca7abb669833b866774
Wes Van der Klomp <wes.vdk@icloud.com>
2017-08-08 18:29:01 +0300

    add position property

--- SpriteKit/Slider/Slider/Label.swift
+++ SpriteKit/Slider/Slider/Label.swift
@@ -12,12 +12,18 @@ import SpriteKit
 /// A label for displaying text on screen.
 struct Label {
     
-    /// The text to be displayed by the label.
+    /// The string that the label displays.
     var text: String
+    
+    /// The position point of the label within it's parent's coordinate system.
+    var position: CGPoint
     
     /// A SKLabelNode for adding to a SpriteKit scene.
     var node: SKLabelNode {
         let n = SKLabelNode()
+        
+        n.text = text
+        n.position = position
         
         return n
     }
