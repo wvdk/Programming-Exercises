65d202cb46d4d3b9b817622828aef5ba53e154bb
Wes Van der Klomp <wes.vdk@icloud.com>
2017-08-08 16:56:17 +0300

    fill out node and position properties of SliderTrack

--- SpriteKit/Slider/Slider/SliderTrack.swift
+++ SpriteKit/Slider/Slider/SliderTrack.swift
@@ -9,6 +9,21 @@
 import Foundation
 import SpriteKit
 
+/// The line for showing the angle the user my drag the knob along.
 struct SliderTrack {
     
+    /// An SKShapeNode for adding to a SpriteKit scene.
+    var node: SKShapeNode {
+        let rect = CGRect(x: position.x, y: position.y, width: 100, height: 10)
+        let n = SKShapeNode(rect: rect, cornerRadius: 4)
+        
+        n.fillColor = SKColor.white
+        n.position = position
+        
+        return n
+    }
+    
+    /// The position of the knob in it's parents coordinate system. Also used to calculate the slider's value.
+    var position: CGPoint
 }
+
