e5a542e6a79fb962b7e78cd0e4b740e74142e626
Wes Van der Klomp <wes.vdk@icloud.com>
2017-08-09 12:54:24 +0300

    lots of reworking - but putting this on hold for a bit

--- Playgrounds/Calculator.playground/Contents.swift
+++ Playgrounds/Calculator.playground/Contents.swift
@@ -1,61 +1,53 @@
+// Calculator
+
+// Brain storming...
+//
+// One appoach could be:
+//
+// 1. Parse the array of string elements into an array of operators and doubles
+// 2. Parse that into an nested array (AST) of "expresions"*
+// 3. Run the AST through a recursive evaluator, returning the final result
+//
+// *An "expresion" is a simple list. The first item of the list represents the operation, followed by the values to be operated on.
+
 import Cocoa
 
-let input = ["1", "×", "2", "sin", "3"]
+let exampleInput = ["1", "×", "2", "sin", "3", "+", "π"]
 
+/// In an expression, anything that is not a number (such as "×" or "sin") is an operation. Non-constant operations perform some kind of calculation on some number of inputs, and return the result.
 enum Operation {
     case constant(Double)
     case unaryOperation((Double) -> Double)
     case binaryOperation((Double, Double) -> Double)
-//    case equals
-//    case clearAll
 }
 
-let operations: Dictionary<String, Operation> = [
-    "π": Operation.constant(Double.pi),
-    "sin": Operation.unaryOperation(sin),
-    "±": Operation.unaryOperation({ -$0}),
-    "×": Operation.binaryOperation({ $0 * $1}),
-    "+": Operation.binaryOperation({ $0 + $1})
+let knownOperators: Dictionary<String, Operation> = [
+    "π": .constant(Double.pi),
+    "sin": .unaryOperation(sin),
+    "±": .unaryOperation({ -$0 }),
+    "×": .binaryOperation({ $0 * $1 }),
+    "+": .binaryOperation({ $0 + $1 })
 ]
 
-func evaluate(input: [String]) -> [String] {
-    
-    if input.count == 1 {
-        return input
-    }
-    
-    var leftSide: Double?
-    var rightSide: Double?
-    
-    for x in input {
-        if let number = Double(x) { // It's a number, not an operator
-            if leftSide == nil {
-                leftSide = number
-            } else {
-                rightSide = number
-            }
-        } else if let op = operations[x] {
-            
-            switch  op {
-            case .constant(<#T##Double#>):
-                <#code#>
-            default:
-                <#code#>
-            }
-            
-            
-            
-            
-            if rightSide != nil {
-                //            do the operation on left and right char and call self with smaller
+/// A collections of elements of an equation that can be evaluated.
+typealias Expression = Array<ExpressionElement>
 
-                
-                
-            }
-        }
-    }
-    
-    return []
+/// An element of an expression that can be either a number or an operation.
+enum ExpressionElement {
+    case operation
+    case number
 }
 
-evaluate(input: input)
+let opop = knownOperators["π"]
+
+if let op = opop {
+
+    switch op {
+    case .constant(let n):
+        print(n)
+    default:
+        print("aaaa")
+    }
+}
+
+
