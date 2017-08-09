4a0640dc61ea01c9551286dc182ee45699cee491
Wes Van der Klomp <wes.vdk@icloud.com>
2017-08-09 21:09:37 +0300

    add node and init method to Slider

--- SpriteKit/Slider/Slider/Slider.swift
+++ SpriteKit/Slider/Slider/Slider.swift
@@ -14,4 +14,15 @@ struct Slider {
     private let knob: SliderKnob
     private let track: SliderTrack
     
+    let node: SKNode
+    
+    init() {
+        knob = SliderKnob(position: CGPoint(x: 0, y: 0))
+        track = SliderTrack(position: CGPoint(x: 0, y: 0))
+        node = SKNode()
+        
+        node.addChild(knob.node)
+        node.addChild(track.node)
+    }
+    
 }
