caefd2e374dc93f27d20c797bb1ceddefed99386
Wes Van der Klomp <wes.vdk@icloud.com>
2017-08-09 13:14:38 +0300

    add recursiveness

--- Playgrounds/Recursive Enum.playground/Contents.swift
+++ Playgrounds/Recursive Enum.playground/Contents.swift
@@ -4,9 +4,13 @@ import Cocoa
 
 enum ArithmeticExpression {
     case number(Int)
-    case addition
-    case multiplication
+    indirect case addition(ArithmeticExpression, ArithmeticExpression)
+    indirect case multiplication(ArithmeticExpression, ArithmeticExpression)
 }
 
 let five = ArithmeticExpression.number(5)
 let four = ArithmeticExpression.number(4)
+let two = ArithmeticExpression.number(2)
+let sumOfFourAndFive = ArithmeticExpression.addition(four, five)
+let productOfSumOfFourAndFiveTimesTwo = ArithmeticExpression.multiplication(sumOfFourAndFive, two)
+
