ac656e77d7dd6c179805e2580894a3738917bc08
Wes Van der Klomp <wes.vdk@icloud.com>
2017-08-09 21:15:04 +0300

    replace knob and track with just a slider

--- SpriteKit/Slider/Slider/GameScene.swift
+++ SpriteKit/Slider/Slider/GameScene.swift
@@ -11,30 +11,29 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    var sliderKnob = SliderKnob(position: CGPoint(x: 0, y: 0))
-    var sliderTrack = SliderTrack(position: CGPoint(x: 0, y: 0))
     var label = Label(text: "123", position: CGPoint(x: 0, y: 100))
+    
+    var slider = Slider()
 
     override func didMove(to view: SKView) {
-        addChild(sliderKnob.node)
-        addChild(sliderTrack.node)
         addChild(label.node)
+        addChild(slider.node)
     }
     
     override func mouseDown(with event: NSEvent) {
-        let xPosition = event.locationInWindow.x
-        
-        sliderKnob.position.x = xPosition
+//        let xPosition = event.locationInWindow.x
+//
+//        sliderKnob.position.x = xPosition
     }
     
     override func mouseDragged(with event: NSEvent) {
-        let xPosition = event.locationInWindow.x
-        
-        sliderKnob.position.x = xPosition
+//        let xPosition = event.locationInWindow.x
+//
+//        sliderKnob.position.x = xPosition
     }
     
     override func mouseUp(with event: NSEvent) {
-        sliderKnob.position = CGPoint(x: 0, y: 0)
+//        sliderKnob.position = CGPoint(x: 0, y: 0)
     }
     
 }
