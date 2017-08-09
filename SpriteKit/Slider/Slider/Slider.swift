2374f5d6f2b5da8b8075c2af2f5208c7ce847086
Wes Van der Klomp <wes.vdk@icloud.com>
2017-08-09 21:15:19 +0300

    add value property and documentation coment

--- SpriteKit/Slider/Slider/Slider.swift
+++ SpriteKit/Slider/Slider/Slider.swift
@@ -9,16 +9,22 @@
 import Foundation
 import SpriteKit
 
+/// A user interface which the user can select and drag a knob to change a value.
 struct Slider {
     
     private let knob: SliderKnob
     private let track: SliderTrack
+    
+//    var valueRange: Range<Int>
+    
+    var value: Int
     
     let node: SKNode
     
     init() {
         knob = SliderKnob(position: CGPoint(x: 0, y: 0))
         track = SliderTrack(position: CGPoint(x: 0, y: 0))
+        value = 50
         node = SKNode()
         
         node.addChild(knob.node)
