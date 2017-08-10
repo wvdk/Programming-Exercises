21f64adf7f3a6a77eaa5078fc804eea958453bf6
Wes Van der Klomp <wes.vdk@icloud.com>
2017-08-10 12:27:50 +0300

    create SliderNode

---
+++ SpriteKit/Slider/Slider/SliderNode.swift
@@ -0,0 +1,14 @@
+//
+//  SliderNode.swift
+//  Slider
+//
+//  Created by Wesley Van der Klomp on 8/10/17.
+//  Copyright © 2017 Wesley Van der Klomp. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class SliderNode: SKShapeNode {
+    
+}
