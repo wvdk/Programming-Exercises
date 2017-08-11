d4b6c4c62e29c37d25b21ee776f7428e5f91f306
Wes Van der Klomp <wes.vdk@icloud.com>
2017-08-11 11:09:07 +0300

    change positions of slider and label and add center property to GameScene

--- SpriteKit/Slider/Slider/GameScene.swift
+++ SpriteKit/Slider/Slider/GameScene.swift
@@ -13,11 +13,15 @@ class GameScene: SKScene {
     
     var label = Label(text: "123", position: CGPoint(x: 0, y: 100))
     var slider = Slider()
-
+    
+    /// Calculated center point of the scene.
+    private var center: CGPoint {
+        return CGPoint(x: size.width / 2, y: size.height / 2)
+    }
 
     override func didMove(to view: SKView) {
-        label.position = CGPoint(x: frame.width / 2, y: frame.height / 2 + 100)
-        slider.position = CGPoint(x: frame.width / 2, y: frame.height / 2)
+        label.position = CGPoint(x: center.x, y: center.y + 100)
+        slider.position = CGPoint(x: center.x - slider.size.width / 2, y: center.y - slider.size.height / 2)
         
         addChild(label.node)
         addChild(slider.node)
