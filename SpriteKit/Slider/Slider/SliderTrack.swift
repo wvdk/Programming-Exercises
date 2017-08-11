c2f766a06ca4d3cfaf4225d8045d4d8811f9a4b4
Wes Van der Klomp <wes.vdk@icloud.com>
2017-08-11 10:52:53 +0300

    create updateNode private func

--- SpriteKit/Slider/Slider/SliderTrack.swift
+++ SpriteKit/Slider/Slider/SliderTrack.swift
@@ -15,7 +15,7 @@ class SliderTrack {
     /// An SKShapeNode for adding to a SpriteKit scene.
     ///
     /// Things like position, size, or color are all updated automatically when you change the properties of a `SliderTrack` instance. So you should rarely need to set properties of this `node` property directly.
-    var node: SKShapeNode
+    var node = SKShapeNode()
     
     /// The position of the knob in it's parents coordinate system. Also used to calculate the slider's value.
     var position: CGPoint  {
@@ -24,10 +24,27 @@ class SliderTrack {
         }
     }
     
+    /// The size of the track.
+    var size = CGSize(width: 300, height: 100) {
+        didSet {
+            updateNode()
+        }
+    }
+    
     /// Creates a track at `position`.
     init(position: CGPoint) {
         self.position = position
         
+        updateNode()
+    }
+    
+
+    /// Creates a track at `position` of (0, 0).
+    convenience init() {
+        self.init(position: CGPoint(x: 0, y: 0))
+    }
+    
+    private func updateNode() {
         let width = 200
         let height = 10
         let rect = CGRect(x: position.x - CGFloat(width / 2), y: position.y - CGFloat(height / 2), width: 200, height: 10)
@@ -37,11 +54,6 @@ class SliderTrack {
         node.fillColor = SKColor.white
         node.fillColor = SKColor.white
         node.position = position
-    }
-    
-    /// Creates a track at `position` of (0, 0).
-    convenience init() {
-        self.init(position: CGPoint(x: 0, y: 0))
     }
     
 }
