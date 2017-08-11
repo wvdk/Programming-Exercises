93284f4a548d84a473b74e07ae493369211ca129
Wes Van der Klomp <wes.vdk@icloud.com>
2017-08-11 20:43:09 +0300

    init project

---
+++ SpriteKit/BasicDraw/BasicDraw/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  BasicDraw
+//
+//  Created by Wesley Van der Klomp on 8/11/17.
+//  Copyright © 2017 Wesley Van der Klomp. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
