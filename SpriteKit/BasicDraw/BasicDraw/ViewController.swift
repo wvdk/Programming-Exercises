aeafc7dd55dc92aa2bb9bea3ae5efaa76aa9a9cb
Wes Van der Klomp <wes.vdk@icloud.com>
2017-08-11 20:49:19 +0300

    remove template crap

--- SpriteKit/BasicDraw/BasicDraw/ViewController.swift
+++ SpriteKit/BasicDraw/BasicDraw/ViewController.swift
@@ -17,18 +17,14 @@ class ViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        if let view = self.skView {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
+        if let view = self.skView {            
+            let scene = GameScene()
+            
+            scene.scaleMode = .aspectFill
+            
+            view.presentScene(scene)
             
             view.ignoresSiblingOrder = true
-            
             view.showsFPS = true
             view.showsNodeCount = true
         }
