ba16352bde504daa05412467eecb423000247b7b
Wes Van der Klomp <wes.vdk@icloud.com>
2017-08-12 07:31:56 +0300

    create Shape stuct

---
+++ SpriteKit/BasicDraw/BasicDraw/Shape.swift
@@ -0,0 +1,14 @@
+//
+//  Shape.swift
+//  BasicDraw
+//
+//  Created by Wesley Van der Klomp on 8/12/17.
+//  Copyright © 2017 Wesley Van der Klomp. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+struct Shape {
+    
+}
