720bc07ebe7346a7cdab78c62f48874c30791308
Wes Van der Klomp <wes.vdk@icloud.com>
2017-08-12 08:43:51 +0300

    add position property

--- SpriteKit/BasicDraw/BasicDraw/Renderer.swift
+++ SpriteKit/BasicDraw/BasicDraw/Renderer.swift
@@ -10,6 +10,7 @@ import Foundation
 
 /// This approach, of seperating the rendering stuff from the data structure stuff, will hopefully enable future renderers such as CoreGraphics or SVG. But more importantly it will also seperate the "how do we render this infinite recursion without crashing the computer" problem out of the data model stuff.
 protocol Renderer {
+    
     associatedtype Render
     
     func render(shape: Shape) -> Render
--- SpriteKit/BasicDraw/BasicDraw/Shape.swift
+++ SpriteKit/BasicDraw/BasicDraw/Shape.swift
@@ -9,7 +9,24 @@
 import Foundation
 import SpriteKit
 
+/*
+
+ Coordinate system:
+ 
+                     (x: 10, y: 10)
+         |-----------
+         |-----------
+         |-----------
+         |___________
+(x: 0, y: 0)
+ 
+ */
 struct Shape {
+    
+    let size: CGSize
+    
+    /// Relative to parent
+    let position: CGPoint
     
     /// An array containing normal Shapes or recursive Shapes
     var children: [Shape] = []
