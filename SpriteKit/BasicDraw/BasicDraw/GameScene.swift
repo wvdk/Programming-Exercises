0f2be904f099f522ddcd1cb6874d61ff87e28e79
Wes Van der Klomp <wes.vdk@icloud.com>
2017-08-12 07:30:18 +0300

    add squareShape

--- SpriteKit/BasicDraw/BasicDraw/GameScene.swift
+++ SpriteKit/BasicDraw/BasicDraw/GameScene.swift
@@ -12,11 +12,21 @@ import GameplayKit
 class GameScene: SKScene {
 
     override func didMove(to view: SKView) {
-        let sideBarContainer = SKShapeNode(rect: CGRect(x: 0, y: 0, width: 100, height: frame.size.height + 1))
+        let sideBarContainer = SKShapeNode(rect: CGRect(x: 0, y: 0, width: 200, height: frame.size.height + 1))
         
         sideBarContainer.fillColor = .white
         
         addChild(sideBarContainer)
+        
+        
+        let squareShape = SKShapeNode(rect: CGRect(x: 0, y: 0, width: 100, height: 100))
+
+        squareShape.fillColor = .red
+        
+        squareShape.position = CGPoint(x: 50, y: frame.size.height - 150)
+        
+        addChild(squareShape)
+        
     }
     
     override func mouseDown(with event: NSEvent) {
