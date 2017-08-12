2e60741135baf360b0990f7dd20d9585d239169e
Wes Van der Klomp <wes.vdk@icloud.com>
2017-08-12 07:52:55 +0300

    create the Renderer protocol

--- SpriteKit/BasicDraw/BasicDraw/SpriteKitRenderer.swift
+++ SpriteKit/BasicDraw/BasicDraw/SpriteKitRenderer.swift
@@ -10,7 +10,6 @@ import Foundation
 
 /// Turns a Shape data structure into an SKNodes hirerarchy
 ///
-/// This approach, of seperating the SpriteKit stuff from the data structure stuff, will hopefully enable future renderers such as CoreGraphics. But it will also seperate the "how do we render this infinite recursion without crashing the computer" problem out of the data model stuff.
 struct SpriteKitRenderer {
     
 }
