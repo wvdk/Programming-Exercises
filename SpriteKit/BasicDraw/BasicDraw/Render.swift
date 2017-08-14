291ac688c491ba10a648af3971abbf8ce532dafe
Wes Van der Klomp <wes.vdk@icloud.com>
2017-08-14 20:36:30 +0300

    create RenderedNode

---
+++ SpriteKit/BasicDraw/BasicDraw/Render.swift
@@ -0,0 +1,14 @@
+//
+//  Render.swift
+//  BasicDraw
+//
+//  Created by Wesley Van der Klomp on 8/14/17.
+//  Copyright © 2017 Wesley Van der Klomp. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class RenderedNode: SKNode {
+    
+}
