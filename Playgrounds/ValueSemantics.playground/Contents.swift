a0f73310b93e3ee959fa0d2724006c59fdf896d4
Wes Van der Klomp <wes.vdk@icloud.com>
2017-08-18 10:51:16 +0300

    create example of how I'd expect it to work

---
+++ Playgrounds/ValueSemantics.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+struct LittleStruct {
+    let constantNumber = 0
+    var variableNumber = 0
+}
+
+let littleStruct1 = LittleStruct()
+let littleStruct2 = LittleStruct(variableNumber: 2)
+
+var littleArray = [littleStruct1, littleStruct2]
+
+print(littleArray)
+
+for i in 0..<littleArray.count {
+    var currentLittleStruct = littleArray[i]
+    
+    currentLittleStruct.variableNumber = 123
+    
+    littleArray[i] = currentLittleStruct
+}
+
+print(littleArray)
