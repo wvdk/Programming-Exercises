e25d29a1f5381155fdfbb9cdc4153fbb76be6713
Wes Van der Klomp <wes.vdk@icloud.com>
2017-09-06 09:56:57 +0300

    edits to MyPlayground

--- Playgrounds/MyPlayground.playground/Contents.swift
+++ Playgrounds/MyPlayground.playground/Contents.swift
@@ -1,3 +1,24 @@
 
+import UIKit
+import PlaygroundSupport
+//
+//let url = URL(string: "https://api.dronestre.am/data")!
+//let task = URLSession.shared.dataTask(with: url, completionHandler: {
+//    (data, response, error) in
+//    print("blah blah blah")
+//    print(response)
+//})
+//
+//task.resume()
+//
+//PlaygroundPage.current.needsIndefiniteExecution = true
+//
+//
+//
+//
+//
 
 
+func yFor(x: Int) -> Int {
+    return 0
+}
