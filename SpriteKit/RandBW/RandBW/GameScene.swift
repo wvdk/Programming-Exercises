2e457e48f1e503f06ad21d295ce4a14437c2d3cd
Wes Van der Klomp <wes.vdk@icloud.com>
2017-12-29 11:34:05 -0800

    create a paddedFrame extension and remove any offscreen child nodes on every update

--- SpriteKit/RandBW/RandBW/GameScene.swift
+++ SpriteKit/RandBW/RandBW/GameScene.swift
@@ -44,6 +44,29 @@ class GameScene: SKScene {
         makeSquares()
     }
 
+    override func update(_ currentTime: TimeInterval) {
+        super.update(currentTime)
+        
+        for child in super.children {
+            if !self.paddedFrame(for: child).contains(child.position) {
+                child.removeFromParent()
+            }
+        }
+    }
+    
 }
 
+extension SKNode {
+    
+    /// Returns a new frame with padding on the top, right, left, bottom by half of the `node`'s  respective height/width.
+    func paddedFrame(for node: SKNode) -> CGRect {
+        let paddedFrame = CGRect(x: self.frame.minX - (node.frame.width / 2),
+                                 y: self.frame.minY - (node.frame.height / 2),
+                                 width: self.frame.width + node.frame.width,
+                                 height: self.frame.height + node.frame.height)
+        
+        return paddedFrame
+    }
+    
+}
 
