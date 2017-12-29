3d1a6a8a2bad7e965e8159bd42a902d250a91188
Wes Van der Klomp <wes.vdk@icloud.com>
2017-12-29 11:36:30 -0800

    Move the paddedFrame extension into it's own file

--- SpriteKit/RandBW/RandBW/GameScene.swift
+++ SpriteKit/RandBW/RandBW/GameScene.swift
@@ -56,17 +56,4 @@ class GameScene: SKScene {
     
 }
 
-extension SKNode {
-    
-    /// Returns a new frame with padding on the top, right, left, bottom by half of the `node`'s  respective height/width.
-    func paddedFrame(for node: SKNode) -> CGRect {
-        let paddedFrame = CGRect(x: self.frame.minX - (node.frame.width / 2),
-                                 y: self.frame.minY - (node.frame.height / 2),
-                                 width: self.frame.width + node.frame.width,
-                                 height: self.frame.height + node.frame.height)
-        
-        return paddedFrame
-    }
-    
-}
 
