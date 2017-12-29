7976bdbb5d0fe3a8ca99de77771a7b5a4b870c88
Wes Van der Klomp <wes.vdk@icloud.com>
2017-12-29 15:00:18 -0800

    mess with the scene size in vc

--- SpriteKit/RandBW/RandBW/GameViewController.swift
+++ SpriteKit/RandBW/RandBW/GameViewController.swift
@@ -12,15 +12,26 @@ import GameplayKit
 
 class GameViewController: UIViewController {
 
+    var scene: GameScene! = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if let view = self.view as! SKView? {
-            let scene = GameScene(size: view.frame.size)
+            scene = GameScene(size: view.frame.size)
             view.presentScene(scene)
             view.ignoresSiblingOrder = true
             view.showsFPS = true
             view.showsNodeCount = true
+            
+            start({ [weak self] in
+                self?.scene.size = CGSize(width: 100, height: 100)
+                
+                start({
+                    guard let viewSize = self?.view.frame.size else { return }
+                    self?.scene.size = viewSize
+                }, after: 2)
+            }, after: 2)
         }
     }
 
