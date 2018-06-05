3e04ec0ffff9c7c1d69a1686bc2fc9502dca888c
Wes Van der Klomp <wes.vdk@icloud.com>
2018-06-04 20:04:45 -0700

    init project

---
+++ WWDC 2018/Playing With Xcode 10 Beta/Playing With Xcode 10 Beta/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Playing With Xcode 10 Beta
+//
+//  Created by Wesley Van der Klomp on 6/4/18.
+//  Copyright © 2018 wes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
