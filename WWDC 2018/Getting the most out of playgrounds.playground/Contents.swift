e31d49416969841fa7071dc694386707d813a0ef
Wes Van der Klomp <wes.vdk@icloud.com>
2018-06-06 14:01:50 -0700

    init project

---
+++ WWDC 2018/Getting the most out of playgrounds.playground/Contents.swift
@@ -0,0 +1,35 @@
+import Cocoa
+import PlaygroundSupport
+
+
+var str = "Hello, playground"
+
+
+/*
+ 
+ 
+PlaygroundPage.current.liveView = // my view controller
+
+ 
+ */
+
+
+
+//: This is markup
+
+
+/*:
+ 
+ So is this
+ 
+ */
+
+
+
+"test"
+do {
+    "test123"
+}
+"test4"
+
+
