8911abab0a51b73989b25559d59437ab288a02ed
Wes Van der Klomp <wes.vdk@icloud.com>
2018-06-21 17:09:04 -0700

    Init project

---
+++ Playgrounds/TestingPlaygrounds/MyCustomFramework/MyCustomClass.swift
@@ -0,0 +1,17 @@
+//
+//  MyCustomClass.swift
+//  TestingPlaygrounds
+//
+//  Created by Wesley Van der Klomp on 6/21/18.
+//  Copyright © 2018 wes. All rights reserved.
+//
+
+import Foundation
+
+class MyCustomClass {
+    
+    static func printMyName() {
+        print("Wes")
+    }
+    
+}
---
+++ Playgrounds/TestingPlaygrounds/TestingPlaygrounds/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestingPlaygrounds
+//
+//  Created by Wesley Van der Klomp on 6/21/18.
+//  Copyright © 2018 wes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
