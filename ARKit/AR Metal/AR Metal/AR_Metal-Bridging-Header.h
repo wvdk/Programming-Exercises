3667086b2693498ad268bb1b3bdf12fcc06ec457
Wes Van der Klomp <wes.vdk@icloud.com>
2018-06-28 11:01:33 -0700

    bring multi user project in

---
+++ ARKit/AR Metal/AR Metal/AR_Metal-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  AR_Metal-Bridging-Header.h
+//  AR Metal
+//
+//  Created by Wesley Van der Klomp on 6/27/18.
+//  Copyright © 2018 wes. All rights reserved.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
