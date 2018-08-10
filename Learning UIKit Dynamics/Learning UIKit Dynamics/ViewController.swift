52b208497105a43af91ec3166e5ab64029228453
Wes Van der Klomp <wes.vdk@icloud.com>
2018-08-09 17:34:21 -0700

    stuff

--- Learning UIKit Dynamics/Learning UIKit Dynamics/ViewController.swift
+++ Learning UIKit Dynamics/Learning UIKit Dynamics/ViewController.swift
@@ -8,9 +8,52 @@
 
 import UIKit
 
+class MoveSupport {
+    
+    var animator : UIDynamicAnimator
+
+    
+    init(moveableView : UIView) {
+        self.animator = UIDynamicAnimator(referenceView: moveableView.superview!)
+
+        let recognizer = UIPanGestureRecognizer(target: self, action: #selector(pan))
+        moveableView.addGestureRecognizer(recognizer)
+    }
+    
+    @objc func pan(_ recognizer : UIPanGestureRecognizer) {
+        let view = recognizer.view!
+        
+        switch(recognizer.state) {
+            
+        case .began:
+            animator.removeAllBehaviors()
+            
+        case .changed:
+            let translation = recognizer.translation(in: view)
+            view.center.x += translation.x
+            view.center.y += translation.y
+            recognizer.setTranslation(.zero, in: view)
+            
+        case .ended:
+            let velocity = recognizer.velocity(in: view)
+            
+            let behavior = UIDynamicItemBehavior(items: [view])
+            behavior.addLinearVelocity(velocity, for: view)
+            behavior.resistance = 3.0
+            animator.addBehavior(behavior)
+            
+        default:
+            break
+        }
+    }
+    
+}
+
 class ViewController: UIViewController {
 
     let redBox = UIView(frame: .zero)
+    var moveSupport: MoveSupport!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,22 +61,8 @@ class ViewController: UIViewController {
         redBox.backgroundColor = .red
         redBox.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
         view.addSubview(redBox)
-        
-        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(pan))
-        redBox.addGestureRecognizer(panGesture)
+        moveSupport = MoveSupport(moveableView: redBox)
     }
     
-    @objc private func pan(_ gesture: UIPanGestureRecognizer) {
-        
-        let translation = gesture.translation(in: redBox)
-        redBox.center.x += translation.x
-        redBox.center.y += translation.y
-        gesture.setTranslation(.zero, in: redBox)
-        
-        
-        
-        print("Pan detected. Translation: \(translation)")
-    }
-
 }
 
