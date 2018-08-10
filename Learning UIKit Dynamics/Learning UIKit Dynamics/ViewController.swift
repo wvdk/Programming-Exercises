14062833c7657ca7e3dad76584d7aea33b9abfa4
Wes Van der Klomp <wes.vdk@icloud.com>
2018-08-09 17:09:36 -0700

    Add pan gesture

--- Learning UIKit Dynamics/Learning UIKit Dynamics/ViewController.swift
+++ Learning UIKit Dynamics/Learning UIKit Dynamics/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     let redBox = UIView(frame: .zero)
+    let panGesture = UIPanGestureRecognizer(target: self, action: #selector(pan))
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +19,14 @@ class ViewController: UIViewController {
         redBox.backgroundColor = .red
         redBox.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
         view.addSubview(redBox)
+        
+        view.addGestureRecognizer(panGesture)
+    }
+    
+    @objc private func pan(_ gesture: UIPanGestureRecognizer) {
+        print("Pan detected.")
+        let translation = gesture.translation(in: view)
+        redBox.center = CGPoint(x: translation.x, y: translation.y)
     }
 
 }
