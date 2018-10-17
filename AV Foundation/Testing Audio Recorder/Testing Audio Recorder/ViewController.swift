bd2853635731d161c0162ed57f2c2fd70f48137d
Wes Van der Klomp <wes.vdk@icloud.com>
2018-10-17 11:30:20 -0700

    set up an audio recorder

--- AV Foundation/Testing Audio Recorder/Testing Audio Recorder/ViewController.swift
+++ AV Foundation/Testing Audio Recorder/Testing Audio Recorder/ViewController.swift
@@ -7,15 +7,43 @@
 //
 
 import UIKit
+import AVFoundation
+
+/// Extension that adds several functions for temporary URLs related to octis
+public extension FileManager {
+    
+    /// Returns the main documents directory for the app
+    ///
+    /// - Returns: The documents directory for the app
+    public func documentDirectory() -> URL {
+        let paths = urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+}
 
 class ViewController: UIViewController {
 
+    var audioRecorder: AVAudioRecorder! = nil
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
-
-
+        let uuid = UUID().uuidString
+        let directory = FileManager.default.documentDirectory().appendingPathComponent("recordings").appendingPathComponent(uuid)
+        if !FileManager.default.fileExists(atPath: directory.path) {
+            try? FileManager.default.createDirectory(at: directory, withIntermediateDirectories: true, attributes: nil)
+            print("Creating a directory for: \(uuid)")
+        }
+        let url = URL(fileURLWithPath: "capturedAudio.m4a", isDirectory: false, relativeTo: directory)
+        
+        audioRecorder = try? AVAudioRecorder(url: url, settings: [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVEncoderBitRateKey: 128000,
+            AVNumberOfChannelsKey: 1,
+            AVSampleRateKey: 44100.0
+        ])
 
     }
 
@@ -28,4 +56,5 @@ class ViewController: UIViewController {
     }
     
 }
+
 
