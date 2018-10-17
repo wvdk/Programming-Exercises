750ad7462a07add2525a1a6c29d08124b8e85ddb
Wes Van der Klomp <wes.vdk@icloud.com>
2018-10-17 11:23:56 -0700

    init project with start / stop buttons

---
+++ AV Foundation/Testing Audio Recorder/Testing Audio Recorder/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Testing Audio Recorder
+//
+//  Created by Wesley Van der Klomp on 10/17/18.
+//  Copyright © 2018 wes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+
+
+
+    }
+
+    @IBAction func startRecording(_ sender: Any) {
+        
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        
+    }
+    
+}
+
