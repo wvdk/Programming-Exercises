7185f8e17222b5d0c43cf99e8d477f4968c3f145
Wes Van der Klomp <wes.vdk@icloud.com>
2019-05-06 08:24:15 -0700

    init project files

---
+++ MyFirstMultipeerConnection/MyFirstMultipeerConnectionMac/MyFirstMultipeerConnectionMac/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MyFirstMultipeerConnectionMac
+//
+//  Created by Wesley Van der Klomp on 5/6/19.
+//  Copyright © 2019 wes. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MyFirstMultipeerConnection/MyFirstMultipeerConnectioniOS/MyFirstMultipeerConnectioniOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFirstMultipeerConnectioniOS
+//
+//  Created by Wesley Van der Klomp on 5/6/19.
+//  Copyright © 2019 wes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
