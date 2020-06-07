aaca9d25e9ea0c691580191d7fd59460bca21ee3
Wes Van der Klomp <wes.vdk@icloud.com>
2020-06-06 18:19:17 -0700

    organizing

---
+++ KnR C/2-1/main.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+
+
+/*
+
+2.2 Data Types and Sizes
+
+Reading notes
+
+Basic data types
+- char
+- int
+- float
+- double
+
+also 
+- long
+- short
+
+
+*/
+
+
+/* Exercise 2-1: Write a program to determine the ranges of char, short, int and long variables, both signed and unsigned, by printing appropriate values from standard headers and by direct computation. Harder if you computer them: determine the ranges of the various floating-point types. */
+int main() {
+	printf("Hello, World!");
+	return 0;
+}
