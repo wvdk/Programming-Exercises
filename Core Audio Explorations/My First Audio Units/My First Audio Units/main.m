aa4dfe064ab360b93b68487c63315019d9be9067
Wes Van der Klomp <wes.vdk@icloud.com>
2020-10-09 19:18:11 -0700

    init new project

---
+++ Core Audio Explorations/My First Audio Units/My First Audio Units/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  My First Audio Units
+//
+//  Created by Wesley Van der Klomp on 10/9/20.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
