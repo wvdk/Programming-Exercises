4207c33f6b5516b57aea9affd57228386fadbdd8
Wes Van der Klomp <wes.vdk@icloud.com>
2020-10-09 20:08:03 -0700

    continue setting up

--- Core Audio Explorations/My First Audio Units/My First Audio Units/main.m
+++ Core Audio Explorations/My First Audio Units/My First Audio Units/main.m
@@ -5,12 +5,44 @@
 //  Created by Wesley Van der Klomp on 10/9/20.
 //
 
-#import <Foundation/Foundation.h>
+#define kInputFileLocation CFSTR("/Users/wes/Developer/Programming-Exercises/Core\ Audio\ Explorations/Piano\ 1.m4a")
 
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
+#import <Foundation/Foundation.h>
+#include <AudioToolbox/AudioToolbox.h>
+
+#pragma mark user-data struct
+typedef struct MyAUGraphPlayer
+{
+    AudioStreamBasicDescription inputFormat;
+    AudioFileID                 inputFile;
+    
+    AUGraph graph;
+    AudioUnit fileAU;
+    
+} MyAUGraphPlayer;
+
+#pragma mark utility functions
+// Insert Listing 4.2 here
+// Insert Listing 7.7 - 7.13 here
+// Insert Listing 7.14 -7.17 here
+
+#pragma mark main function
+int    main(int argc, const char *argv[])
+{
+    // Open the input audio file
+    // Get the audio data format from the file
+    // Insert Listing 7.3 here
+    
+    // Build a basic fileplayer->speakers graph
+    // Configure the file player
+    // Insert Listing 7.4 here
+    
+    // Start playing
+    // Sleep until the file is finished
+    // Insert Listing 7.5 here
+    
+    // Cleanup
+    // Insert Listing 7.6 here
+    
     return 0;
 }
