c6b08497b38468ebdd9b7b9525a85f4a53062741
Wes Van der Klomp <wes.vdk@icloud.com>
2020-10-11 11:31:52 -0700

    Add CheckError function

--- Core Audio Explorations/My First Audio Units/My First Audio Units/main.m
+++ Core Audio Explorations/My First Audio Units/My First Audio Units/main.m
@@ -22,7 +22,29 @@ typedef struct MyAUGraphPlayer
 } MyAUGraphPlayer;
 
 #pragma mark utility functions
-// Insert Listing 4.2 here
+
+static void CheckError(OSStatus error, const char *operation)
+{
+    if (error == noErr) return;
+    
+    char errorString[20];
+    // See if it appears to be a 4-char-code
+    *(UInt32 *)(errorString + 1) = CFSwapInt32HostToBig(error);
+    if (isprint(errorString[1]) && isprint(errorString[2]) &&
+        isprint(errorString[3]) && isprint(errorString[4])) {
+        
+        errorString[0] = errorString[5] = '\'';
+        errorString[6] = '\0';
+    } else {
+        // No, format it as an integer
+        sprintf(errorString, "%d", (int)error);
+    }
+    
+    fprintf(stderr, "Error: %s (%s)\n", operation, errorString);
+    
+    exit(1);
+}
+
 // Insert Listing 7.7 - 7.13 here
 // Insert Listing 7.14 -7.17 here
 
@@ -31,7 +53,17 @@ int    main(int argc, const char *argv[])
 {
     // Open the input audio file
     // Get the audio data format from the file
-    // Insert Listing 7.3 here
+    CFURLRef inputFileURL = CFURLCreateWithFileSystemPath(
+                                  kCFAllocatorDefault,
+                                  kInputFileLocation,
+                                  kCFURLPOSIXPathStyle,
+                                  false);
+    
+    MyAUGraphPlayer player = {0};
+    
+    // Open the input audio file
+    CheckError(AudioFileOpenURL(inputFileURL, kAudioFileReadPermission, 0, &player.inputFile), "AUdioFileOpenURL failed");
+    CFRelease(inputFileURL);
     
     // Build a basic fileplayer->speakers graph
     // Configure the file player
