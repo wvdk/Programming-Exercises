682ff9071c2a5c09fbdff7195c36461043df3ea6
Wes Van der Klomp <wes.vdk@icloud.com>
2020-11-30 19:11:28 -0800

    init MyCoreAudioTools project

---
+++ Core Audio Explorations/MyCoreAudioTools/MyCoreAudioTools/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MyCoreAudioTools
+//
+//  Created by Wesley Van der Klomp on 11/30/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Core Audio Explorations/MyCoreAudioTools/MyCoreAudioTools/GetFileMetadata.m
@@ -0,0 +1,8 @@
+//
+//  GetFileMetadata.m
+//  MyCoreAudioTools
+//
+//  Created by Wesley Van der Klomp on 11/30/20.
+//
+
+#import <Foundation/Foundation.h>
