2576ad9aa1ea987d6589860fd037c63d9a558543
Wes Van der Klomp <wes.vdk@icloud.com>
2020-12-24 14:38:49 -0800

    styling and layout

--- Scrundinger/Scrumdinger/MeetingView.swift
+++ Scrundinger/Scrumdinger/MeetingView.swift
@@ -9,7 +9,20 @@ import SwiftUI
 
 struct MeetingView: View {
     var body: some View {
-        ProgressView(value: 5, total: 15)
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                    Label("600", systemImage: "hourglass.tophalf.fill")
+                }
+            }
+        }
     }
 }
 
