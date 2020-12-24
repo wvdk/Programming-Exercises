d7a3ee57939404991bf6fb9712ba87279188106b
Wes Van der Klomp <wes.vdk@icloud.com>
2020-12-24 13:22:01 -0800

    init SwiftUI tutorial project

---
+++ Scrundinger/Scrundinger/ScrundingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrundingerApp.swift
+//  Scrundinger
+//
+//  Created by Wesley Van der Klomp on 12/24/20.
+//
+
+import SwiftUI
+
+@main
+struct ScrundingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
