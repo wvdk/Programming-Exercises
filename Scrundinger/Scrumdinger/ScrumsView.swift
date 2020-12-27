2daaf56253e4c9fae22774da7b66f437e80793b3
Wes Van der Klomp <wes.vdk@icloud.com>
2020-12-27 15:49:24 -0800

    Create ScrumsView.swift

---
+++ Scrundinger/Scrumdinger/ScrumsView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct ScrumsView: View {
+    var body: some View {
+        Text("hi")
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView()
+    }
+}
