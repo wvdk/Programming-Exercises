2e8a4b38022f6763753934ac9a5159cc5c064848
Wes Van der Klomp <wes.vdk@icloud.com>
2020-12-27 16:30:19 -0800

    building out the DetailView

--- Scrundinger/Scrumdinger/DetailView.swift
+++ Scrundinger/Scrumdinger/DetailView.swift
@@ -1,13 +1,33 @@
 import SwiftUI
 
 struct DetailView: View {
+    let scrum: DailyScrum
+    
     var body: some View {
-        Text("hi")
+        List {
+            Section(header: Text("Meeting Info")) {
+                Label("Start Meeting", systemImage: "timer")
+                    .font(.headline)
+                    .foregroundColor(.accentColor)
+                    .accessibilityLabel(Text("Start meeting"))
+                HStack {
+                    Label("Length", systemImage: "clock")
+                        .accessibilityLabel(Text("Meeting length"))
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+                HStack {
+                    Label("Color", systemImage: "paintpalette")
+                    Spacer()
+                    Image(systemName: "checkmark.circle.fill")
+                }
+            }
+        }
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView()
+        DetailView(scrum: DailyScrum.data[0])
     }
 }
