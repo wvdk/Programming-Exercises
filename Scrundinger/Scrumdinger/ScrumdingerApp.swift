94748fe5f9e8b30f3da3ae6bcee693f42b015dc0
Wes Van der Klomp <wes.vdk@icloud.com>
2020-12-27 16:21:50 -0800

    Set up the navigation

--- Scrundinger/Scrumdinger/ScrumdingerApp.swift
+++ Scrundinger/Scrumdinger/ScrumdingerApp.swift
@@ -1,17 +1,12 @@
-//
-//  ScrumdingerApp.swift
-//  Scrumdinger
-//
-//  Created by Wesley Van der Klomp on 12/24/20.
-//
-
 import SwiftUI
 
 @main
 struct ScrumdingerApp: App {
     var body: some Scene {
         WindowGroup {
-            MeetingView()
+            NavigationView {
+                ScrumsView(scrums: DailyScrum.data)
+            }
         }
     }
 }
