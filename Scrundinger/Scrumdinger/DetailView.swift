cf3d004cc5afacd70f5b66f64d62fbcfd4438107
Wes Van der Klomp <wes.vdk@icloud.com>
2020-12-27 16:23:31 -0800

    init DetailView

---
+++ Scrundinger/Scrumdinger/DetailView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text("hi")
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
