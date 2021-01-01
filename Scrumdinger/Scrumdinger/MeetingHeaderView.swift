689a5b190c81acb82937783c2d2860bd31f62902
Wes Van der Klomp <wes.vdk@icloud.com>
2020-12-31 16:46:46 -0800

    Break out MeetingView's header into its own view (MeetingHeaderView.swift).

---
+++ Scrumdinger/Scrumdinger/MeetingHeaderView.swift
@@ -0,0 +1,39 @@
+import SwiftUI
+
+struct MeetingHeaderView: View {
+    @Binding var secondsElapsed: Int
+    @Binding var secondsRemaining: Int
+    private var progress: Double {
+        guard secondsRemaining > 0 else { return 1 }
+        let totalSeconds = Double(secondsElapsed + secondsRemaining)
+        return Double(secondsElapsed) / totalSeconds
+    }
+    var body: some View {
+        VStack {
+            ProgressView(value: progress)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("\(secondsElapsed)", systemImage: "hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("\(secondsRemaining)", systemImage: "hourglass.tophalf.fill")
+                }
+            }
+        }
+        .accessibilityElement(children: .ignore)
+        .accessibilityLabel(Text("Time remaining"))
+        .accessibilityValue(Text("10 minutes"))
+    }
+}
+
+struct MeetingHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingHeaderView(secondsElapsed: .constant(60), secondsRemaining: .constant(180))
+            .previewLayout(.sizeThatFits)
+    }
+}
