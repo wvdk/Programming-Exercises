151c959fad64d902672aa661aa70e84bb935a0c9
Wes Van der Klomp <wes.vdk@icloud.com>
2021-01-31 15:25:27 -0800

    init Project

---
+++ KnR C/5.12/C Delcaration Parser/C Delcaration Parser/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C Delcaration Parser
+//
+//  Created by Wesley Van der Klomp on 1/31/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
